2e4a517daddc3dc5bdc00b18e4d08b5d6a434413
GABRIEL CARDOSO LIMA <gabriel.clima15@senacsp.edu.br>
2022-07-21 22:10:19 -0300

    att 21.07.2022

--- login/src/components/Login/Login.jsx
+++ login/src/components/Login/Login.jsx
@@ -13,6 +13,12 @@ export function Login() {
     password: ''
   })
 
+  const [status, setStatus] = useState({
+    type:'',
+    mensagem: '',
+    loading: false
+  })
+
   const valorInput = e => setUser({
       ...user,
      [e.target.name]: e.target.value
@@ -25,14 +31,31 @@ export function Login() {
       const headers = {
         content_type: 'application/json'
       }
+
+      setStatus({
+        loading: true
+      })
+
       await api.post('/login', user, {headers})
       .then((response) =>{
-        console.log(response)
+        // console.log(response)
+        setStatus({
+          type: 'success',
+          mensagem: response.data.mensagem,
+          loading: false
+        })
       }).catch((error) =>{
+        setStatus({
+          type: 'error',
+          mensagem: 'erro: tente mais tarde!',
+        })
         if(error.response){
-          console.log(error.response)
-        } else {
-          cosnole.log('Erro: Tente mais tarde...')
+          // console.log(error.response)
+          setStatus({
+            type: 'error',
+            mensagem: error.response.data.mensagem,
+            loading: false
+          })
         }
       })
     }
@@ -41,8 +64,11 @@ export function Login() {
     <div className="box">
       {/* <Container className="box"> */}
       <Form onSubmit={loginSubmit} className="borderForm">
+      {status.type == 'error' ? <h3 className="p-alert-error">{status.mensagem}</h3>: ''}
+      {status.type == 'success' ? <h3 className="p-alert-success">{status.mensagem}</h3>: ''}
+      {status.loading ? <h3 className="p-alert-validando">Validando...</h3>: ''}
         <div className="user">
-          <UserCircle size={60} color="#030202" />{" "}
+          <UserCircle size={80} color="#030202" />{" "}
         </div>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label className="FormLabel">
